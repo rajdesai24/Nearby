9c90709271dfdff1ce365da9e948c50b746a99ae
ShahMugdha <54849579+ShahMugdha@users.noreply.github.com>
2021-01-09 00:53:24 +0530

    Create dashboard.js

---
+++ nearby/frontend/src/Components/Screens/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Parts/Dashboard.css";
+import UserNav from "../Parts/UserNav";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+const dashboard=()=>{
+    return(
+        <>
+            <UserNav/>
+            
+        </>
+    )
+}
+export default dashboard;
