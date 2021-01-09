7e6e1f1ff90591ad7887ee417d5eabe59d13c281
ShahMugdha <54849579+ShahMugdha@users.noreply.github.com>
2021-01-09 16:55:36 +0530

    Create mapsDash.js

---
+++ nearby/frontend/src/Components/Parts/mapsDash.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { useScrollTrigger } from "@material-ui/core";
+
+import Box from "@material-ui/core/Box";
+
+const MapContainer = (props) => {
+  const mapStyles = {
+    width: "100%",
+    height: "100%",
+  };
+  return (
+    <Map
+      google={props.google}
+      zoom={14}
+      style={mapStyles}
+      center={{
+        lat: props.lat,
+        lng: props.long,
+      }}
+    >
+      {props.list.map((listElement) => {
+        console.log(listElement);
+        return (
+          <Marker
+            position={{
+              lat: listElement[1],
+              lng: listElement[0],
+            }}
+            label={listElement[4]}
+            onClick={() => {
+              console.log("marker clicked");
+              props.selectedInfo(listElement);
+            }}
+          />
+        );
+      })}
+    </Map>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+})(MapContainer);
